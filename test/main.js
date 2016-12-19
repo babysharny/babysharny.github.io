a2f6c864df0ead5fbb8e3f373574fd25f86e99b6
Simon Radik <babysharny@gmail.com>
2016-12-19 22:46:50 +0300

    Select

---
+++ test/main.js
@@ -0,0 +1,37 @@
+
+    $('.selectpicker').ready(function () {
+
+        console.log('selectpicker is ready');
+
+        var countries = ['Country1', 'Country2'];
+
+        countries.reverse();
+
+        countries.reverse().forEach(function (city) {
+            addValue(city);
+        });
+
+        $('.selectpicker').selectpicker("refresh");
+
+
+        // var value = '';
+        $('.bs-searchbox > input').on('input', function (event) {
+            $('.selectpicker').selectpicker({
+                noneResultsText: 'press \'Enter\' to select \'' + event.currentTarget.value + '\''
+            });
+        });
+
+        $('.bs-searchbox > input').on('keydown', function (event) {
+            if(event.key == 'Enter') {
+                console.log(event);
+                addValue(event.currentTarget.value);
+                $('.selectpicker').selectpicker("refresh");
+            }
+        });
+
+        function addValue(value) {
+            $('.selectpicker').append('<option value="'+value+'" selected="">'+value+'</option>');
+        }
+
+
+    });
